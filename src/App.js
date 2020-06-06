a8936ee8e422778e5ac18e3782588b1f3c4249c3
Priyanshi Patel <silver.puzzle.10@gmail.com>
2020-06-06 18:36:49 -0400

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import React, { Component } from 'react'
-import Form from './components/form'
+import Search from './components/binary-search'
 
 class App extends Component {
-  
-
 
   render() {
     return (
       <div className="App">
-        <Form/>
-    </div>
+        <Search/>
+      </div>
     )
   }
 }
