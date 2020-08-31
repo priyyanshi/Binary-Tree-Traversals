dfc65fcf66a1daec90bcf051eeeec25ba2815f12
Priyanshi Patel <priyanshi.patel@hotmail.com>
2020-08-31 01:18:25 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-This project was made using create-react-app. It allows the user to visualize data structures and sorting algorithms.
-
-View at: https://data-structures-and-algo.herokuapp.com/
---
+++ precache-manifest.e07454a77254791e547964c7a5fda738.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a304b7cbe0ca6d2925176145685eb12a",
+    "url": "/Data-structures-and-algos/index.html"
+  },
+  {
+    "revision": "2f23950f045db3dd49fe",
+    "url": "/Data-structures-and-algos/static/css/2.26884a19.chunk.css"
+  },
+  {
+    "revision": "7042ae3b6cb4876ac881",
+    "url": "/Data-structures-and-algos/static/css/main.4625d086.chunk.css"
+  },
+  {
+    "revision": "2f23950f045db3dd49fe",
+    "url": "/Data-structures-and-algos/static/js/2.a5c72ce7.chunk.js"
+  },
+  {
+    "revision": "4fed55ec31b38f113a525eb575ca3b09",
+    "url": "/Data-structures-and-algos/static/js/2.a5c72ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7042ae3b6cb4876ac881",
+    "url": "/Data-structures-and-algos/static/js/main.f63c6036.chunk.js"
+  },
+  {
+    "revision": "484d8b663acadd28d587",
+    "url": "/Data-structures-and-algos/static/js/runtime-main.e475e4c9.js"
+  }
+]);
